b094c2926761fc5565e779dd5b57d7d458170a03
aimee rivers <aimee@aimeerivers.com>
2026-02-21 23:43:48 +0100

    chore: Try SMTP authentication method: login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
     port: 587,
     user_name: ENV["BREVO_SMTP_USERNAME"],
     password: ENV["BREVO_SMTP_KEY"],
-    authentication: :plain,
+    authentication: :login,
     enable_starttls_auto: true
   }
 
