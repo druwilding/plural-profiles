4f88f66a79cca4db4fde2d2d1cb6b65cc39a54ae
Drue Wilding <drue@wilding.dk>
2026-02-24 17:51:50 +0100

    feat: Support the HTML details and summary tags
    Extend Rails simple_format to allow for the details and summary tags, and the open attribute

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  DESCRIPTION_EXTRA_TAGS = %w[details summary].to_set.freeze
+  DESCRIPTION_EXTRA_ATTRIBUTES = %w[open].to_set.freeze
+
+  def formatted_description(text)
+    safe_list_class = self.class.safe_list_sanitizer.class
+    tags = safe_list_class.allowed_tags + DESCRIPTION_EXTRA_TAGS
+    attrs = safe_list_class.allowed_attributes + DESCRIPTION_EXTRA_ATTRIBUTES
+    simple_format(text, {}, sanitize_options: { tags: tags, attributes: attrs })
+  end
 end
