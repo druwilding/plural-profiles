f5758f02f08ee81bfa36d93e81fc04de22e406fa
druwilding <drue+github@wilding.dk>
2026-03-10 18:55:41 +0100

    chore: Nullify the theme foreign key on delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_08_191720) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_10_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -154,5 +154,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_08_191720) do
   add_foreign_key "profiles", "users"
   add_foreign_key "sessions", "users"
   add_foreign_key "themes", "users"
-  add_foreign_key "users", "themes", column: "active_theme_id"
+  add_foreign_key "users", "themes", column: "active_theme_id", on_delete: :nullify
 end
