ba0d1248e556bbd39aa2d75bff174f89169059ca
druwilding <drue+github@wilding.dk>
2026-03-15 14:12:46 +0100

    test: Save minitest reports

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,22 +3,28 @@ require_relative "../config/environment"
 require "rails/test_help"
 require_relative "test_helpers/session_test_helper"
 
-if ENV["CI"].present?
-  require "minitest/reporters"
-  Minitest::Reporters.use! [
-    Minitest::Reporters::DefaultReporter.new(color: true),
-    Minitest::Reporters::HtmlReporter.new(reports_dir: "test/reports")
-  ]
-end
-
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers (single worker in CI so reporters work)
-    parallelize(workers: ENV["CI"].present? ? 1 : :number_of_processors)
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
   end
+end
+
+if ENV["CI"].present?
+  require "minitest/reporters"
+  require "minitest/minitest_reporter_plugin"
+  Minitest::Reporters.use!(
+    [
+      Minitest::Reporters::DefaultReporter.new(color: true),
+      Minitest::Reporters::HtmlReporter.new(reports_dir: "test/reports")
+    ],
+    ENV,
+    Minitest.backtrace_filter
+  )
+  Minitest.extensions << "minitest_reporter" unless Minitest.extensions.include?("minitest_reporter")
 end
