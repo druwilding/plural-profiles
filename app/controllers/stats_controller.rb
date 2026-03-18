4c2af04d162ab2cfba8cea0978eee3322b0cc156
Drue Wilding <drue@hedia.com>
2026-03-18 09:48:56 +0100

    fix: Remove deactivated users from the stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,6 @@
 class StatsController < ApplicationController
   def index
-    @user_count = User.count
+    @user_count = User.where(deactivated_at: nil).count
     @profile_count = Profile.count
     @group_count = Group.count
     @avatar_count = ActiveStorage::Attachment.where(name: "avatar", record_type: [ "Profile", "Group" ]).count
