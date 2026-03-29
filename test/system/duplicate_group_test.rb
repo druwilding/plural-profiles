94b6f5f5a3a7158dc06f812d0eade9c897a68220
druwilding <drue+github@wilding.dk>
2026-03-29 15:11:16 +0200

    test: Fix another flaky system test

--- test/system/duplicate_group_test.rb
+++ test/system/duplicate_group_test.rb
@@ -274,6 +274,7 @@ class DuplicateGroupTest < ApplicationSystemTestCase
     click_button "Next"
 
     assert_text "Confirm duplication"
+    assert_current_path duplicate_confirm_our_group_path(prism), ignore_query: true
     click_button "Confirm and duplicate"
     assert_text "Group duplicated"
 
